24b01240af31dd12972806c1676506ebf8e593d7
Sam Lin <sam0817@gmail.com>
2022-04-17 00:11:01 +0800

    grpc test

---
+++ GrpcClient/Controllers/WeatherForecastController.cs
@@ -0,0 +1,48 @@
+using Grpc.Net.Client;
+using GrpcGreeter;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GrpcClient.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+    }
+
+    [HttpGet]
+    [Route("GrpcCall")]
+    public async Task<string> CallGrpc()
+    {
+        using var channel = GrpcChannel.ForAddress("https://localhost:7007");
+        var client = new Greeter.GreeterClient(channel);
+        var reply = await client.SayHelloAsync(
+            new HelloRequest { Name = "GreeterClient" });
+        Console.WriteLine("Greeting: " + reply.Message);
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+        return reply.Message;
+    }
+}
